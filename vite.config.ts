5593c457be204162c13fab34cb9e706648448f3e
Valyriancode1 <gulzar.nadeem@outlook.com>
2026-04-28 20:19:42 +0930

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwind from "@tailwindcss/vite";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/AMNursinghomeandmaternitycenter-2770/",
-  plugins: [react(), tailwind()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/web"),
-    },
-  },
-  build: {
-    outDir: "dist",
-  },
-});
+  base: '/AMNursinghomeandmaternitycenter-2770/',
+  plugins: [react()],
+})
