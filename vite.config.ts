4ae3d5590524d157c6c1a6c3501c3c6826162baa
Valyriancode1 <gulzar.nadeem@outlook.com>
2026-04-28 20:47:40 +0930

    restore tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwind from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/AMNursinghomeandmaternitycenter-2770/',
-})
+  base: "/AMNursinghomeandmaternitycenter-2770/",
+  plugins: [react(), tailwind()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/web"),
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+});
